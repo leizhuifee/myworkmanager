61927670a69ad4bc69a30a57248aa871715209a8
leizhuifee <leizhuifee@163.com>
2020-07-15 21:04:28 +0800

    071515

--- MyWorkManager/Startup.cs
+++ MyWorkManager/Startup.cs
@@ -28,7 +28,7 @@ namespace MyWorkManager
             {
                 options.AutomaticAuthentication = false;
             });
-            services.AddControllersWithViews();//зЂВс
+           
             services.AddDbContext<MyworkContext>(optionsAction: options => { options.UseSqlServer(Configuration.GetConnectionString(name: "LocalDB")); });
             services.AddTransient<Servers.ITicketRepository, Servers.TicketRepository>();
             services.AddScoped<Servers.IHomeimg, Servers.HomeimgRepositorye>();
@@ -38,8 +38,7 @@ namespace MyWorkManager
                  );
 
             services.AddMvc(options => { options.Filters.Add(new Microsoft.AspNetCore.Mvc.AutoValidateAntiforgeryTokenAttribute()); });
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
-            services.Configure<IdentityOptions>(options =>
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
             {
                 options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
@@ -53,10 +52,28 @@ namespace MyWorkManager
                 options.Lockout.MaxFailedAccessAttempts = 5;
 
                 options.User.RequireUniqueEmail = false;
-                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            }).AddEntityFrameworkStores<IdentityDbContext>().AddDefaultTokenProviders();
+            //services.Configure<IdentityOptions>(
+            //    options =>
+            //    {
+            //        options.Password.RequireDigit = false;
+            //        options.Password.RequireLowercase = false;
+            //        options.Password.RequireNonAlphanumeric = false;
+            //        options.Password.RequireUppercase = false;
+            //        options.Password.RequiredLength = 6;
+            //        options.Password.RequiredUniqueChars = 1;
 
+            //        options.Lockout.AllowedForNewUsers = true;
+            //        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMilliseconds(5);
+            //        options.Lockout.MaxFailedAccessAttempts = 5;
 
-            });
+            //        options.User.RequireUniqueEmail = false;
+            //        options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+            //    });
+            services.AddControllersWithViews();//зЂВс
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
