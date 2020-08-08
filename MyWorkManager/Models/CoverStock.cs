f9d51651773f9d664e9664635c193e7788d5fb92
leizhuifee <leizhuifee@163.com>
2020-08-08 19:36:17 +0800

    11

--- MyWorkManager/Models/CoverStock.cs
+++ MyWorkManager/Models/CoverStock.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +9,17 @@ namespace MyWorkManager.Models
 {
     public class CoverStock
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string Colour { get; set; }
         
-       
+       [Required]
         public string Sleeve { get; set; }
+        [Required]
         public string Size { get; set; }
+        [Required]
         public int Number { get; set; }
     }
 }
